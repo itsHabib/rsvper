0f283e1b5292facda6db0099c112e9686a302b07
Michael Habib <michael.habib@couchbase.com>
2023-03-13 17:18:36 -0500

    add twilio for text messages

--- cmd/rsvp-lambda/main.go
+++ cmd/rsvp-lambda/main.go
@@ -7,6 +7,8 @@ import (
 	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/twilio/twilio-go"
+	twilioApi "github.com/twilio/twilio-go/rest/api/v2010"
 
 	"github.com/itsHabib/rsvper/internal/cfa"
 	"github.com/itsHabib/rsvper/internal/scheduler"
@@ -26,11 +28,25 @@ func HandleLambdaEvent(ctx context.Context, event scheduler.TaskRequest) (string
 	}
 	s.SetCookie(event.CFACookie)
 
+	twilioClient := twilio.NewRestClient()
+	params := &twilioApi.CreateMessageParams{}
+	params.SetTo("+18186247532")
+	params.SetFrom("+15075017519")
+
 	status, err := s.PollRSVP(ctx, event.Schedule)
 	if err != nil {
+		params.SetBody(fmt.Sprintf("unable to poll rsvp: %v", err))
+		if _, smsErr := twilioClient.Api.CreateMessage(params); smsErr != nil {
+			fmt.Printf("unable to send sms: %s", err)
+		}
 		return "", fmt.Errorf("unable to poll rsvp: %w", err)
 	}
+
 	fmt.Printf("rsvp status: %s\n", status.String())
+	params.SetBody("successfully submitted rsvp request, with rsvp status: " + status.String())
+	if _, smsErr := twilioClient.Api.CreateMessage(params); smsErr != nil {
+		fmt.Printf("unable to send sms: %s", err)
+	}
 
 	return status.String(), nil
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,11 @@ go 1.18
 require (
 	github.com/aws/aws-lambda-go v1.38.0
 	github.com/aws/aws-sdk-go v1.44.219
+	github.com/twilio/twilio-go v1.3.5
 )
 
-require github.com/jmespath/go-jmespath v0.4.0 // indirect
+require (
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
